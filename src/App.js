532d680b84f3e3f0c5c1c5142f9eceff9d1234a0
Musa Salumu <musasalumu@Musas-MacBook-Pro.local>
2020-07-27 13:38:29 +0300

    finalizing  basic pokemon game

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Pokedex from './Pokedex';
 
 function App() {
   return (
     <div className='App'>
-      <p>Pokemon</p>
+      <Pokedex />
     </div>
   );
 }
--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import './Pokecard.css';
 class Pokecard extends Component {
   render() {
+    const { name, type, base_experience, id } = this.props.poke;
+    const pokeUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+
     return (
-      <div>
-        <h1>POKECARD</h1>
+      <div className='Pokecard'>
+        <h1 className='Pokecard-title'>{name}</h1>
+        <img src={pokeUrl} alt={name} />
+        <div className='Pokecard-data'>Type:{type}</div>
+        <div className='Pokecard-data'>EXP:{base_experience}</div>
       </div>
     );
   }

0af73f0a5d46ee379e0a619a55fb0ae9a70bbee8
Musa Salumu <musasalumu@Musas-MacBook-Pro.local>
2020-07-27 12:38:45 +0300

    Initilize Pokecard and pokedex file

---
+++ src/Pokecard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class Pokecard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>POKECARD</h1>
+      </div>
+    );
+  }
+}
+
+export default Pokecard;
---
+++ src/Pokedex.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Pokedex extends Component {
+  render() {
+    return (
+      <div>
+        <h1>POKEDEX</h1>
+      </div>
+    );
+  }
+}
+
+export default Pokedex;
